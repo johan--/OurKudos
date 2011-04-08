1f1706bb0028dccc3e58e29b2fe9cfa2288399e3
Marcin Walczak <marcin.walczak@gmail.com>
2011-04-08 21:11:32 +0200

    Migration file

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,11 @@ class User < ActiveRecord::Base
   
   
   def generate_api_key key_length = 64
-    if self.api_key.blank?
-      big_array = ('A'..'Z').to_a + ("a".."z").to_a + ("0".."9").to_a
-      self.api_key = ''
-      1.upto(key_length) { self.api_key << big_array[rand(big_array.size-1)] }
-    end
-    self.api_key
+    return self.api_key unless self.api_key.blank?
+     
+    big_array = ('A'..'Z').to_a + ("a".."z").to_a + ("0".."9").to_a
+    self.api_key = ''
+    1.upto(key_length) { self.api_key << big_array[rand(big_array.size-1)] }
   end
   
   
---
+++ features/users.feature
@@ -0,0 +1,4 @@
+Scenario: User has automatically generated api key after regristration
+
+Given there are no users yet
+When I go to the home page
