fdcb33f548d24ec9dd27772f773f793e38fa80b4
Marcin Walczak <marcin.walczak@gmail.com>
2011-04-11 17:12:10 +0200

    Added Our Kudos library rspec tests

--- spec/our_kudos_spec.rb
+++ spec/our_kudos_spec.rb
@@ -6,12 +6,20 @@ describe OurKudos::Client do
         instance = OurKudos::Client.new
         instance.should be_a(OurKudos::Client)
       end
+      
+      it 'should be able to assign an api key' do
+        OurKudos::Client.respond_to? "api_key=" should_not be_false
+      end
+      
     end
     
     context "given an instance" do
       let(:client) { OurKudos::Client.new }
 
-
+        it 'should be able to assign an api key' do
+          client.api_key == 'new_api_key'
+          client.api
+        end
 
     end
     
