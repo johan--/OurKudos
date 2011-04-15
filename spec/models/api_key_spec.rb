c891a8d24c5c5b4e2fe93ae15bc12a46ef2b68c7
Marcin Walczak <marcin.walczak@gmail.com>
2011-04-15 16:38:27 +0200

    More rspec tests

--- spec/models/api_key_spec.rb
+++ spec/models/api_key_spec.rb
@@ -1,5 +1,57 @@
 require 'spec_helper'
 
 describe ApiKey do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context 'given an instance' do 
+    let(:api_key) { ApiKey.new(:site_id => 1) } 
+    
+    it 'should be able to generate an string key before save' do 
+      new_api_key = ApiKey.new
+      new_api_key.key.should be_blank
+      new_api_key.save
+      new_api_key.key.should_not be_blank
+    end
+    
+    it 'should be able to re-generate given api string' do
+      api_key.should respond_to 'regenerate!'
+      api_key.save
+      
+      old_key = api_key.key
+      
+      api_key.regenerate!
+      
+      api_key.key.should_not == old_key
+      
+    end
+    
+    it 'should be able to set given api key as expired' do
+      api_key.should respond_to 'set_as_expired!'
+      api_key.should respond_to 'expired?'
+      
+      api_key.expires_at.should be_nil
+      
+      api_key.set_as_expired!
+      
+      api_key.expires_at.should_not be_nil
+      api_key.expired?.should be_true
+      
+    end
+    
+    it 'should be able to mark given api key as valid if it is expired' do 
+      api_key.should respond_to 'set_as_valid!'
+      
+      api_key.expires_at.should be_nil
+      
+      api_key.expired?.should be_false
+      
+    end
+    
+    
+    
+    
+    
+    
+  end
+  
+  
 end
