bd4377ec4e04296f5f7b65a19bbb7f94c310c18e
Ben Liu - iMac <blzabub8@gmail.com>
2011-04-19 15:16:12 -0400

    fix typo locakble => lockable
    Signed-off-by: Ben Liu - iMac <blzabub8@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :token_authenticatable, :confirmable, :locakble, :timeoutable
+         :token_authenticatable, :confirmable, :lockable, :timeoutable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
   
