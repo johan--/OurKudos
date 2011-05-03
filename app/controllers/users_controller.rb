fbd458a32123e6116bf7432e02f9db2a94a824ac
Marcin Walczak <marcin.walczak@gmail.com>
2011-05-03 13:32:47 +0200

    Admin area can be accessed after sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,22 +7,5 @@ class UsersController < ApplicationController
     @authentications = current_user.authentications
   end
   
-  def twitter_email
-    @user = User.new
-    @user.apply_omniauth(session[:omniauth])
-    if request.post? 
-      @user.email = params[:user][:email] if params[:user]
-      if @user.save         
-        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => 'twitter'
-        sign_in_and_redirect(:user, @user)
-        session[:omniauth] = nil #removes data from session after save
-      else
-        render :action => :twitter_email
-      end  
-    end
-  end
-  
-  
-  
   
 end
