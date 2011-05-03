915cfd521134d436e7f8b5b63927d9bf84182a40
Marcin Walczak <marcin.walczak@gmail.com>
2011-05-03 10:29:12 +0200

    Facebook authorization works

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,46 +1,63 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_filter :set_omniauth_data
   
-  def method_missing provider
-    if !User.omniauth_providers.index(provider).blank?
+  attr_accessor :omniauth_data
+  attr_accessor :preexisting_authorization_token
+  
+  def method_missing(provider)
+    return super unless valid_provider?(provider)
+    omniauthorize_additional_account || omniauth_sign_in || omniauth_sign_up
+  end
 
-      omniauth = env["omniauth.auth"]
-    
-      if current_user
-        current_user.authentications.find_or_create_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
-         flash[:notice] = "Authentication successful"
-         redirect_to edit_user_registration_path
-      else
-    
-      authentication = Authentication.find_by_provider_and_uid omniauth['provider'], omniauth['uid']
-   
-        if authentication
-          flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => omniauth['provider']
-          #sign_in_and_redirect :user, authentication.user
-          sign_in_and_redirect authentication.user, :event => :authentication
-        else
-          
-          unless omniauth.recursive_find_by_key("email").blank?
-            user = User.find_or_initialize_by_email :email => omniauth.recursive_find_by_key("email")
-          else
-            user = User.new
-          end
-          
-          user.apply_omniauth(omniauth)
-       
-          user.confirm! unless user.email.blank?
+  def omniauth_sign_in
+    return false unless preexisting_authorization_token
 
-          if user.save 
-            flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => omniauth['provider'] 
-            sign_in_and_redirect(:user, user)
-          else
-            session[:omniauth] = omniauth.except('extra')
-            flash[:notice] = I18n.t 'devise.oauth.information.missing'
-            redirect_to new_user_registration_path(:autofill => "true")
-          end
-          
-        end
-      end
+    flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => omniauth_data['provider']
+    sign_in_and_redirect(:user, preexisting_authorization_token.user)
+    true
+  end
+
+  def omniauth_sign_up
+    unless omniauth_data.recursive_find_by_key("email").blank?
+      user = User.find_or_initialize_by_email(:email => omniauth_data.recursive_find_by_key("email"))
+    else
+      user = User.new
     end
+
+    user.apply_omniauth(omniauth_data)
+
+    if user.save 
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => omniauth_data['provider'] 
+      sign_in_and_redirect(:user, user)
+    else
+      session[:omniauth] = omniauth_data.except('extra')
+      flash[:notice] = I18n.t 'devise.oauth.information.missing'
+      redirect_to new_user_registration_path(:autofill => "true")
+    end
+  end
+
+  def omniauthorize_additional_account
+    return false if current_user.nil?
+    if preexisting_authorization_token && preexisting_authorization_token != current_user
+      flash[:alert] = "You have created two accounts and they can't be merged automatically. Email support@ourkudos.com for help."
+      sign_in_and_redirect(:user, current_user)
+    else
+
+      current_user.apply_omniauth(omniauth_data)
+      current_user.save
+
+      flash[:notice] = "Account connected"
+      sign_in_and_redirect(:user, current_user)
+    end
+  end
+
+  def set_omniauth_data
+    self.omniauth_data = env["omniauth.auth"]
+    self.preexisting_authorization_token = Authentication.find_by_provider_and_uid(omniauth_data['provider'], omniauth_data['uid'])
+  end
+
+  def valid_provider?(provider)
+    !User.omniauth_providers.index(provider).nil?
   end
   
 end
