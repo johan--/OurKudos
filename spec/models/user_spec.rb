0f65a4771dbb676cb772bf5ba8165101ef184c54
Marcin Walczak <marcin.walczak@gmail.com>
2011-05-04 12:42:03 +0200

    Specs for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,50 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+ 
+
+  context 'given an instance' do 
+  let(:user) { User.new(:first_name =>"marcin", :last_name => "Walczak") } 
+  
+  let(:omniauth) do
+    {"provider"  => "facebook",
+      "uid"       => '12345',
+      "user_info" => {
+        "email" => "marcin.walczak@gmail.com",
+        "first_name" => "Marcin",
+        "last_name"  => "Walczak",
+        "name"       => "Marcin Walczak"
+      },
+      'credentials'=> {
+        'token' => 'facebook token'
+      }
+    }
+  end  
+    
+    it 'should be able to return first and last name' do
+      user.should respond_to 'to_s' 
+      user.to_s.include?("marcin").should be_true 
+      user.to_s.include?("Walczak").should be_true
+    end
+    
+    it 'should be able to set user attributes from session data' do
+      u = User.new
+      u.apply_omniauth(omniauth)
+      u.first_name.should == 'Marcin'
+      u.last_name.should == 'Walczak'
+      u.email.should == 'marcin.walczak@gmail.com'
+      authentication = u.authentications.first
+      authentication.should_not be_blank
+      authentication.token.should == 'facebook token'
+      authentication.uid.should == '12345'
+      authentication.provider.should == 'facebook'
+    end
+    
+    
+    
+  end  
+  
+  
+
 end
