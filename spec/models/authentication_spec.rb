219370d8fe69fddfc7474011763a9e96308b88ed
Marcin Walczak <marcin.walczak@gmail.com>
2011-05-12 19:14:14 +0200

    auth spec file

---
+++ spec/models/authentication_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Authentication do
+  
+  it 'should be able to get all its types' do
+    Authentication.should respond_to 'options_for_provider'
+    Authentication.options_for_provider.should be_an_instance_of Array
+    Authentication.options_for_provider.first.should be_an_instance_of Array
+  end
+
+   context 'given an instance' do
+
+    let(:auth) { Authentication.new }
+
+    it 'should be invalid without provider type, uid, or token' do
+     auth.valid?.should be_false
+    end
+
+    it 'should be valid with provider type, uid, and token' do
+     auth.token    = 'some token'
+     auth.uid      = 'some uid'
+     auth.provider = 'some provider'
+     auth.valid?.should be_true
+    end
+
+
+   end
+end
