9808acc889185b795ace614806470c76a79a9e7b
Marcin Walczak <marcin.walczak@gmail.com>
2011-05-16 13:58:30 +0200

    Removed authentications from users profile/ moved to admin area

--- app/controllers/admin/authentications_controller.rb
+++ app/controllers/admin/authentications_controller.rb
@@ -1,17 +1,29 @@
 class Admin::AuthenticationsController < Admin::AdminController
   before_filter :authenticate_user!
+  before_filter :get_user
+
+  def new
+    @user.authentications.new
+  end
   
-  
+  def create
+    @authentication = @user.authentications.new params[:authentication]
+    if @authentication.save 
+      redirect_to admin_user_path(@user), :notice => t(:authentication_has_been_added)
+    else  
+      render :action => :new
+    end  
+  end
+
   def edit
     @authentication = Authentication.find params[:id]
-    @submit_url = admin_user_authentication_path(@authentication.user, @authentication)
-    render :template => "/authentications/edit"
+    @submit_url = admin_user_authentication_path(@user, @authentication)
   end
   
   def update
     @authentication = Authentication.find params[:id]
     if @authentication.update_attributes params[:authentication] 
-      redirect_to admin_user_path(@authentication.user, @authentication), :notice => t(:authentication_has_been_updated)
+      redirect_to admin_user_path(@user, @authentication), :notice => t(:authentication_has_been_updated)
     else  
       render :action => :edit
     end
@@ -20,12 +32,16 @@ class Admin::AuthenticationsController < Admin::AdminController
   def destroy
     @authentication = Authentication.find params[:id]
     if @authentication.destroy
-      redirect_to admin_user_path(@authentication.user), :notice => t(:authentication_has_been_removed)
+      redirect_to admin_user_path(@user), :notice => t(:authentication_has_been_removed)
     else
-      redirect_to admin_user_path(@authentication.user), :notice => t(:unable_to_remove_authentication)
+      redirect_to admin_user_path(@user), :notice => t(:unable_to_remove_authentication)
     end
   end
   
-  
+    private
+
+    def get_user
+      @user = User.find params[:user_id]
+    end
   
 end
