ba164317dc604ff90774ec942a3e77c5ba119dfd
Marcin Walczak <marcin.walczak@gmail.com>
2011-05-17 17:15:53 +0200

    Validators fixed

--- app/models/authentication_observer.rb
+++ app/models/authentication_observer.rb
@@ -1,6 +1,6 @@
 class AuthenticationObserver < ActiveRecord::Observer
 
-  def after_save authentication
+  def after_save authentication    
     if authentication.provider == 'twitter' && Identity.find_by_identity_type_and_identity('twitter', authentication.nickname).blank?
       authentication.user.identities.create :identity_type => "twitter",
                                             :identity      => authentication.nickname
--- lib/validators/twitter_validator.rb
+++ lib/validators/twitter_validator.rb
@@ -1,8 +1,7 @@
 class TwitterValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    record.errors[attribute] << (options[:message] || I18n.t(:please_authorize_with_twitter_first)) unless record.user.has_twitter_handle?(value)
-    record.errors[attribute] << (options[:message] || I18n.t(:twitter_identity_already_exists)) unless record.user.has_twitter_handle?(value).blank?
+    record.errors[attribute] << (options[:message] || I18n.t(:please_authorize_with_twitter_first)) unless record.user.has_twitter_handle?(value)    
   end
 
 
