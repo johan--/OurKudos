b9b15cc939781bfd7b617a8c1fa278c0c4b5a26a
Marcin Walczak <marcin.walczak@gmail.com>
2011-05-17 19:44:48 +0200

    Code refactored

--- lib/lockable.rb
+++ lib/lockable.rb
@@ -7,13 +7,15 @@ module OurKudos
 
     def set_lock_time
       return self.unlock_in = Date.today-100.years if self.failed_attempts == 0 || self.failed_attempts < @@max_attempts
-      ary = []
-      1.step(@@max_attempts*10,0.8) {|i| ary << i}
-
-      self.unlock_in = Time.now + ((self.failed_attempts*2)*(ary[self.failed_attempts-@@max_attempts])).seconds
+      self.unlock_in = Time.now + lock_seconds
     end
 
+    def lock_seconds
+      ary = []
+      1.step(@@max_attempts*10,0.5) {|i| ary << i}
 
+      return (self.failed_attempts*2)*(ary[self.failed_attempts-@@max_attempts])
+    end
 
   end
 end
