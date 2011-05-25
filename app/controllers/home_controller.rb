37666f67a200c6951333803793d14c3206eb30e8
Ben Liu - iMac <blzabub8@gmail.com>
2011-05-25 09:39:46 -0400

    added a before_filter authenticate_user check on the home#home action to protect the registered user homepage from unsigned in users. added a redirect from the unregistered user homepage to the registered user homepage if user is already signed in. added route for home#home action named 'home_path', added a sign out link to the home view, rest of the content to come.
    Signed-off-by: Ben Liu - iMac <blzabub8@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
 	layout 'main'
-	
+	before_filter :authenticate_user!, :except => [:index]
+  
   def index
-    
+			redirect_to home_path if user_signed_in?
   end    
   
   def home
