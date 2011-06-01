e58695ae0292b16d0b9189870a8111f3cfebd0c8
Marcin Walczak <marcin@marcin-envy.(none)>
2011-06-01 13:07:23 +0200

    Fixed typo

--- app/models/email_kudo.rb
+++ app/models/email_kudo.rb
@@ -1,4 +1,4 @@
 class EmailKudo < ActiveRecord::Base
   has_one :kudo,           :as => :kudoable,    :dependent => :destroy
-  has_one  :confirmations, :as => :confirmable, :dependent => :destroy
+  has_one  :confirmation, :as => :confirmable, :dependent => :destroy
 end
