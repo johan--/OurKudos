26e10ba5b04a3c620ae72a41aeec9c710edf56f2
Marcin Walczak <marcin@marcin-envy.(none)>
2011-06-01 20:25:45 +0200

    Added helper to switch backgrounds

--- app/helpers/kudos_helper.rb
+++ app/helpers/kudos_helper.rb
@@ -1,8 +1,14 @@
 module KudosHelper
 
-  def kudos_current_class_for tab
-     params[:kudos] == tab ? "active kdt_#{tab}" : ''
+  def kudos_current_class_for_link tab
+     return 'active' if tab == 'sent' && params[:kudos].blank?
+
+     params[:kudos] == tab ? 'active' : ''
   end
 
+  def kudos_current_class_for_tab
+    return "kdt_sent" if params[:kudos].blank? || params[:kudos] == "sent"
+    "kdt_#{params[:kudos]}"
+  end
 
 end
