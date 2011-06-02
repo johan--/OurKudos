02fc7f2355447ef5fa96a7ff2880984803ab24fd
Marcin Walczak <marcin.walczak@gmail.com>
2011-06-02 18:18:22 +0200

    Email added to scope

--- lib/facebook.rb
+++ lib/facebook.rb
@@ -1,9 +1,18 @@
 module OurKudos
   module Facebook
 
-    def authenticate_with authentication
-      user = FbGraph::User.new 'matake', :access_token => YOUR_ACCESS_TOKEN)
-    end
+   def fb_post!(message, name=DEFAULT_FB_POST_NAME,
+          description="TODO", url=DEFAULT_SHARE_URL, img=DEFAULT_FB_SHARE_IMAGE)
+          options = {'access_token' => facebook_auth.token,
+               'message' => message,
+               'link' => url,
+               'picture' => img,
+               'name' => name,
+               'caption' => url,
+               'description' => description
+    }
+    RestClient.post(URI.escape("https://graph.facebook.com/me/feed/"), options)
+  end
 
     def auth
 
