3cd185f1a6770acb5228313e5e31cae722dfdc55
Marcin Walczak <marcin.walczak@gmail.com>
2011-06-03 09:30:00 +0200

    Moved email kudos to background processor for test

--- app/models/kudo_copy_observer.rb
+++ app/models/kudo_copy_observer.rb
@@ -1,7 +1,7 @@
 class KudoCopyObserver < ActiveRecord::Observer
 
   def after_create record
-    UserNotifier.kudo(record.kudoable).deliver! if record.kudoable.is_a?(EmailKudo)
+    UserNotifier.delay.kudo(record.kudoable) if record.kudoable.is_a?(EmailKudo)
     record.author.friendships.find_by_friend_id(record.recipient.id).update_friendship_statistics
   end
 
