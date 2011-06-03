cd5926e82b9069af2e2091b81a0b1f66c1aa1f87
Marcin Walczak <marcin.walczak@gmail.com>
2011-06-03 12:25:08 +0200

    More caching improvements for facebook friends module

--- lib/facebook.rb
+++ lib/facebook.rb
@@ -7,17 +7,17 @@ module OurKudos
 
    def fetch_and_save_friends
      facebook_user.friends do |friend|
-       friend.fetch
-       facebook_friends.create :first_name => friend.first_name,
-                               :last_name  => friend.last_name,
-                               :name       => friend.name,
-                               :identifier => friend.identifier
+       facebook_friend = friend.fetch
+       facebook_friends.create :first_name => facebook_friendss.first_name,
+                               :last_name  => facebook_friend.last_name,
+                               :name       => facebook_friend.name,
+                               :identifier => facebook_friend.identifier
 
      end if connected_with?(:facebook)
    end
 
   def facebook_auth
-    @facebook_auth ||= self.authentications.find_by_provider 'facebook'
+    @facebook_auth ||=self.authentications.find_by_provider 'facebook'
   end
 
   def connected_with? provider
