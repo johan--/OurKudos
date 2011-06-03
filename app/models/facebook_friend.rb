d3e732b6a40e9aa2e04e04c14cba92e7f577221a
Marcin Walczak <marcin.walczak@gmail.com>
2011-06-03 16:02:06 +0200

    Encapsulated call with FacebooKFriend model method

--- app/models/facebook_friend.rb
+++ app/models/facebook_friend.rb
@@ -1,3 +1,11 @@
 class FacebookFriend < ActiveRecord::Base
   belongs_to :user
+
+
+  class << self
+
+    def fetch_for user
+      Delayed::Job.enqueue FacebookFriendsFetchJob.new user
+    end
+  end
 end
