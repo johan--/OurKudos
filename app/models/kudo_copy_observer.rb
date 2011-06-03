e30aad83be28697e2c9536fa5e0b1b1395350d02
Marcin Walczak <marcin.walczak@gmail.com>
2011-06-03 09:35:27 +0200

    Fixed bug with Email Kudo

--- app/models/kudo_copy_observer.rb
+++ app/models/kudo_copy_observer.rb
@@ -1,8 +1,11 @@
 class KudoCopyObserver < ActiveRecord::Observer
 
   def after_create record
-    UserNotifier.delay.kudo(record.kudoable) if record.kudoable.is_a?(EmailKudo)
-    record.author.friendships.find_by_friend_id(record.recipient.id).update_friendship_statistics
+    if record.kudoable.is_a?(EmailKudo)
+      UserNotifier.delay.kudo(record.kudoable)
+    else
+      record.author.friendships.find_by_friend_id(record.recipient.id).update_friendship_statistics
+    end
   end
 
 end
