146811069508cba6607e909623199d32548105d0
Marcin Walczak <marcin.walczak@gmail.com>
2011-06-14 15:19:18 +0200

    Fixed one validation error

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ActiveRecord::Base
   # ================
   validates :first_name, :presence => true
   validates :last_name,  :presence => true
-  validates :email,      :presence => true, :uniqueness => true, :email => true
+  validates :email,      :presence => true, :email => true
   validates :password,   :presence => true, 
                          :format   => { :with => RegularExpressions.password },
                          :is_forbidden_password => true,
