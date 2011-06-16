92afcc3a36fa0c9bae020e06dc0665b4e02c1815
Marcin Walczak <marcin.walczak@gmail.com>
2011-06-16 11:55:06 +0200

    More improvements to sign in process with identities (skipped unconfirmed identities)

--- lib/devise/database_authenticatable_with_identities.rb
+++ lib/devise/database_authenticatable_with_identities.rb
@@ -4,16 +4,28 @@ module Devise
   module Strategies
 
     class DatabaseAuthenticatableWithIdentities < Authenticatable
-      def authenticate!
-        resource = valid_password? && Identity.find_by_identity_and_identity_type(authentication_key.gsub(/^@{1}/,''), get_identity_type).user rescue nil
 
-        if validate(resource){ resource.valid_password?(password) }
-          resource.after_database_authentication
-          success!(resource)
-        elsif !halted?
+      attr_reader :identity
+
+      def authenticate!
+        @identity = Identity.find_by_identity_and_identity_type(authentication_key.gsub(/^@{1}/,''), get_identity_type)
+
+        if identity_is_invalid?
+          save_email_for_pass_recovery
           fail(:invalid)
+        else
+
+          resource = valid_password? && identity.user rescue nil
+
+          if validate(resource){ resource.valid_password?(password) }
+            resource.after_database_authentication
+            success!(resource)
+          elsif !halted?
+            save_email_for_pass_recovery
+            fail(:invalid)
+          end
         end
-        session['user.email_for_password_recovery'] = authentication_hash[:email] if authentication_hash[:email] =~ RegularExpressions.email
+
       end
 
       def authentication_key
@@ -24,6 +36,16 @@ module Devise
         return "email"   if authentication_key =~ RegularExpressions.email
         "twitter" if authentication_key =~ RegularExpressions.twitter
       end
+
+      def save_email_for_pass_recovery
+        session['user.email_for_password_recovery'] = authentication_hash[:email] if authentication_hash[:email] =~ RegularExpressions.email
+      end
+
+      def identity_is_invalid?
+        @identity.blank? || (@identity && !@identity.confirmation.confirmed?)
+      end
+
+
     end
   end
 end
