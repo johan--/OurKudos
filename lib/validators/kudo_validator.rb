fb8cb43c03fa080ecef88649a7d6245084b71cb9
Marcin Walczak <marcin.walczak@gmail.com>
2011-06-17 16:57:39 +0200

    Disable kudo sending to empty recipient

--- lib/validators/kudo_validator.rb
+++ lib/validators/kudo_validator.rb
@@ -2,6 +2,7 @@ class KudoValidator < ActiveModel::Validator
 
   def validate record
     record.errors[:base] << I18n.t(:cannot_send_kudos_to_yourself_sorry, :own_identity => record.author_as_recipient_readable_list) if record.author_as_recipient?
+    record.errors[:base] << I18n.t(:you_cannot_send_an_empty_kudo)
   end
 
 
