7fb666c913af9ba95e5a46ec41207bcae52fcc4b
Marcin Walczak <marcin.walczak@gmail.com>
2011-06-23 13:55:38 +0200

    Changed password wording, shortened email kudo string verifier

--- app/models/email_kudo.rb
+++ app/models/email_kudo.rb
@@ -3,12 +3,16 @@ class EmailKudo < ActiveRecord::Base
 
   has_one :kudo, :class_name => "KudoCopy", :as => :kudoable
 
-  before_save :generate
+  before_save :generate_16
 
   scope :date_range, ->(from, to) { where(:created_at => from..to) }
 
   def viewable_by_recipient? email, kudo_id
     email == self.email  && kudo_id == self.key
+  end
+
+  def generate_16
+    generate 16
   end
 
 end
