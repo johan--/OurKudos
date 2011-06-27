9f76e8931f9f5fa8e41af0edca38d3955e97dad7
Marcin Walczak <marcin.walczak@gmail.com>
2011-06-27 08:03:57 +0200

    Admin kudos sorting

--- app/controllers/admin/kudo_flags_controller.rb
+++ app/controllers/admin/kudo_flags_controller.rb
@@ -1,10 +1,34 @@
 class Admin::KudoFlagsController < Admin::AdminController
 
+  helper_method :sort_column, :sort_direction
+
   def index
-    @kudo_flag = KudoFlag.scoped.order("id DESC").
-                          page(params[:page]).
-                          per(25)
+    @kudo_flags = KudoFlag.joins(:flagger).
+                          joins(:kudo).
+                          order("#{sort_column} #{sort_direction}").
+                          page(params[:page]).per(25)
   end
+
+  private
+
+
+  private
+
+    def sort_column
+      case params[:column]
+        when
+        'kudo_flags.created_at',
+        'kudos.body',
+        'users.last_name',
+        'kudo_flags.flag_reason'
+        else
+          'kudo_flags.created_at'
+      end
+    end
+
+    def sort_direction
+      %{asc desc}.include?(params[:direction].to_s) ? params[:direction] : "asc"
+    end
 
 
 end
