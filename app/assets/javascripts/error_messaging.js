d72d39921bf6a0b6a82a053970f537bc21d1d560
Ben Liu - iMac <blzabub8@gmail.com>
2011-06-28 11:33:16 -0400

    implementing new dialog design by Sean
    adding necessary graphics, background wave form graphic is stubbed awaiting wider or repeating background
    
    adding necessary stylesheet rule additions
    
    adding necessary javascript changes: we need to add certain DOM elements to the jquery-ui implementation, but we need to do this selectively based on what type of dialog box is being presented. Instead of tinkering directly inside jquery-ui code, we're manipulating the DOM after jqueryui has made its calls
    
    Signed-off-by: Ben Liu - iMac <blzabub8@gmail.com>

--- app/assets/javascripts/error_messaging.js
+++ app/assets/javascripts/error_messaging.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
 	// if there are Rails flash alerts or Devise form errors, capture the HTML and insert into interstitial message area
 	
 	if ($('#error_explanation').length) {
-		$('#error_explanation').dialog({modal: true, resizable: false });
+		$('#error_explanation').dialog({modal: true, resizable: false, title: 'Action Required:' });
+		$('.ui-dialog').prepend('<div id="dialog-kudos-character" class="stopcop"></div>');
+		$('.ui-dialog-content').prepend('<div class="flow-around-object"></div>');
 	}
 	
 	if ($('.alert').length) {
-		$('.alert').dialog({modal: true, resizable: false });
+		$('.alert').dialog({modal: true, resizable: false, title: 'Alert:' });
+		$('.ui-dialog').prepend('<div id="dialog-kudos-character" class="stopcop"></div>');
+		$('.ui-dialog-content').prepend('<div class="flow-around-object"></div>');
 	}
 
 	
@@ -14,6 +18,9 @@ $(document).ready(function(){
   $('a.video_tour_link').click(function(){
   	$('body').append('<div id="kudos_video"></div>');
   	$('#kudos_video').html('<iframe width="425" height="349" src="http://www.youtube.com/embed/ahHZFck-2ys?rel=0" frameborder="0" allowfullscreen></iframe>').dialog({modal: true, width: 467, resizable: false, title: 'Take the OurKudos Video Tour' });
+  	/* fix some styling rules that really apply to error messages */
+  	$('span.ui-dialog-title').css('width','auto');
+  	$('.ui-widget-header').css('backgroundImage','none');
   	
   	/* close button on jquery ui modal dialog clicked, remove the video content from the DOM */
   	$('a.ui-dialog-titlebar-close').click(function(){
