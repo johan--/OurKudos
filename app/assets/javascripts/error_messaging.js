652b11769d147d02854c8562f0569e87f6f795d8
Ben Liu - iMac <blzabub8@gmail.com>
2011-06-29 12:16:05 -0400

    fixing some IE8/IE7 bugs
    We cannot add class: "class_name" to button config arrays in jquery ui dialogs. IE8/7 throw exceptions and the entire javascript file is ignored. Instead we use jquery to add the class after the object has been inserted into the DOM. Fixes just about all problems except some button classes in IE7 when there are two buttons on the dialog.

--- app/assets/javascripts/error_messaging.js
+++ app/assets/javascripts/error_messaging.js
@@ -2,22 +2,27 @@ $(document).ready(function(){
 	// if there are Rails flash alerts or Devise form errors, capture the HTML and insert into interstitial message area
 	
 	if ($('#error_explanation').length) {
-		$('#error_explanation').dialog({width: 446, modal: true, resizable: false, title: 'Action Required:', draggable: false, buttons: [{ text: "Okay", click: function(){$(this).dialog("close");}, class: 'action_button' } ] });
+		$('#error_explanation').dialog({width: 446, modal: true, resizable: false, title: 'Action Required:', draggable: false, buttons: [{ text: "Okay", click: function(){$(this).dialog("close");} } ] });
 		$('.ui-dialog').prepend('<div id="dialog-kudos-character" class="stopcop"></div>');
 		$('.ui-dialog-content').prepend('<div class="flow-around-object"></div>');
+		// we style the button this way because doing it as a button config option breaks in IE8
+		$('button.ui-button').addClass('action_button');
 	}
 	
 	if ($('.alert').length) {
-		$('.alert').dialog({width: 446, modal: true, resizable: false, title: 'Alert:', draggable: false, buttons: [{ text: "Okay", click: function(){$(this).dialog("close");}, class: 'action_button' } ]});
+		$('.alert').dialog({width: 446, modal: true, resizable: false, title: 'Alert:', draggable: false, buttons: [{ text: "Okay", click: function(){$(this).dialog("close");} } ]});
 		$('.ui-dialog').prepend('<div id="dialog-kudos-character" class="stopcop"></div>');
 		$('.ui-dialog-content').prepend('<div class="flow-around-object"></div>');
+		$('button.ui-button').addClass('action_button');
 	}
 
 	
 	// the video tour button clicked (move this to a separate file at some point)
   $('a.video_tour_link').click(function(){
   	$('body').append('<div id="kudos_video"></div>');
-  	$('#kudos_video').html('<iframe width="425" height="349" src="http://www.youtube.com/embed/ahHZFck-2ys?rel=0" frameborder="0" allowfullscreen></iframe>').dialog({modal: true, width: 467, resizable: false, buttons: [{ text: "Exit", click: function(){$(this).dialog("close");}, class: 'action_button' } ]  });
+  	$('#kudos_video').html('<iframe width="425" height="349" src="http://www.youtube.com/embed/ahHZFck-2ys?rel=0" frameborder="0" allowfullscreen></iframe>').dialog({modal: true, width: 467, resizable: false, buttons: [{ text: "Exit", click: function(){$(this).dialog("close");} } ]  });
+  	
+  	$('button.ui-button').addClass('action_button');
   	/* fix some styling rules that really apply to error messages */
   	
   	$('.ui-widget-header').css('display','none');
@@ -32,9 +37,7 @@ $(document).ready(function(){
   	return false;
   });
   
- 	$('a#cancel-kudo-flag').click(function(){
- 		$('div').dialog('close');
- 	});
+
         
 	
 
