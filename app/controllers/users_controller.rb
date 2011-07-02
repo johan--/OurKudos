7a21fc271a16a9dafc0cf74038eb8ba225d742fe
Charley Stran <charley.stran@gmail.com>
2011-07-01 21:29:50 -0500

    ability to edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,17 @@ class UsersController < ApplicationController
     @kudos           = current_user.inbox.kudos.page(params[:page]).per(5)
   end
   
+  def edit
+    @user = User.find(current_user.id)
+  end
   
+  def update
+    @user = current_user
+    @user.skip_password_validation = true
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :notice  => "Successfully updated user."
+    else
+      render :action => 'edit'
+    end
+  end
 end
