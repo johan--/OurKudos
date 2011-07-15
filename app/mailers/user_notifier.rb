33e9d096feee77a0fdb814e92a96cc50134443b3
Ben Liu - iMac <blzabub8@gmail.com>
2011-07-15 12:56:09 -0400

    adding plaintext alternative email template for identity confirmation

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -26,6 +26,7 @@ class UserNotifier < ActionMailer::Base
 
     mail :to => @email, :subject => subject do |format|
       format.html { render template }
+      format.text { render template }
     end
     
   end
