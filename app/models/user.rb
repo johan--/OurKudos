f5ba189e0e7a41ee908a458d537ec3c0101ba3cd
Ben Liu - iMac <blzabub8@gmail.com>
2011-07-18 12:17:56 -0400

    remove birthday field from registration form remove validation requirement for birthday field

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,7 +49,6 @@ class User < ActiveRecord::Base
   validates :password,   :format   => { :with => RegularExpressions.password },
                          :is_forbidden_password => true,
                          :confirmation => true, :unless => :skip_password_validation
-  validates :birthday,   :presence => true
   # ================
   # == extensions ==
   # ================
