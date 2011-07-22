a7e6b9271a61ee56209ad4e5ddcdb10344003ba7
Charley Stran <charley.stran@gmail.com>
2011-07-21 19:08:09 -0500

    add abilities for gift editor role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,11 @@ class Ability
         can :manage, :all
       elsif user.has_role?(:editor)
         can :manage, KudoFlag
+      elsif user.has_role?(:gift)
+        can :manage, Gift
+        can :manage, GiftGroup
+        can :manage, Merchant
+        can :manage, AffiliateProgram
       end
   end
 
