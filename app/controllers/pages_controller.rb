07073d64822d6d83a10719e88bdddb57ea462642
Marcin Walczak <marcin.walczak@gmail.com>
2011-07-22 20:54:21 +0200

    CMS for page footer links done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,10 @@
 class PagesController < ApplicationController
+  before_filter :authenticate_user!
+  layout :choose_layout
+
+  def show
+    @page = Page.find_with_locale params[:id], I18n.locale
+    render_404 and return if @page.blank?
+  end
+
 end
