456e5a84b5460b917476ff5901be01f83ac9d569
Marcin Walczak <marcin.walczak@gmail.com>
2011-07-26 20:09:37 +0200

    More user profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,25 @@
 class ProfilesController < ApplicationController
+
   layout 'unregistered'
 
   def show
+    debugger
     redirect_to home_path if user_signed_in?
+
     @user = User.find params[:user_id]
+    get_public_kudos
+  end
+
+  private
+
+    def get_public_kudos
+      %w{received sent}.include?(params[:kudos]) ?
+          term = params[:kudos] : term = "newsfeed"
+
+      @kudos = @user.send("#{term}_kudos").page(params[:page]).per(10)
+      @kudos = Kudo.public_kudos.limit(10)         if term == 'received' && @kudos.blank?
+      @kudos = @kudos.order("kudos.id DESC")       if @kudos.respond_to?(:order) && @kudos.first.is_a?(Kudo)
+      @kudos = @kudos.order("kudo_copies.id DESC") if @kudos.respond_to?(:order) && @kudos.first.is_a?(KudoCopy)
   end
 
 
--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -69,3 +69,4 @@ class UserNotifier < ActionMailer::Base
 
 
 end
+x
