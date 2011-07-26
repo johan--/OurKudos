28c21c54c1b67327296aaba205ee5375d989cbf5
Charley Stran <charley.stran@gmail.com>
2011-07-25 20:34:18 -0500

    moved social buttons into a hover

--- app/assets/javascripts/social.js
+++ app/assets/javascripts/social.js
@@ -1,39 +1,53 @@
 
 jQuery(document).ready(function(){
  ////////////////////// 
-  $("#social_buttons a").click(function(event) {
+  //$("#social_buttons a").click(function(event) {
+  $(".kudo_message_cont").hover(function(event) {
+    ////
     var base_url = "www.rkudos.com/kudos/"
-    var kudo_id = $(this).attr('name');
+    //var kudo_id = $(this).attr('name');
+    kudo_id = $(".kudo_message_mid", this).data('id');
 
-    $(this).empty();
-    //////START GOOGLE//////
-    gapi.plusone.render(this, {"size" : "small", "count" : "false", "href" : base_url + kudo_id});
-    //////END GOOGLE//////
-    $('html').attr("xmlns:og","http://www.facebook.com/2008/fbml").attr("xmlns:fb","http://www.facebook.com/2008/fbml");
+    var google_div = "google_plus_" + kudo_id
+    if ($("#" + google_div).is(':empty')) {
+      $(google_div).empty();
+      //////START GOOGLE//////
+      gapi.plusone.render(google_div, {"size" : "medium", "count" : "false", "href" : base_url + kudo_id});
+      //////END GOOGLE//////
+      $('html').attr("xmlns:og","http://www.facebook.com/2008/fbml").attr("xmlns:fb","http://www.facebook.com/2008/fbml");
 
-    // Remove previously created FB like elements -- if they exist -- so they can be re-added after AJAX pagination
-    $('.fb-like').remove();
-    $('#fb-root').empty();
+      // Remove previously created FB like elements -- if they exist -- so they can be re-added after AJAX pagination
+      $('.fb-like').remove();
+      $('#fb-root').empty();
 
-    // Build and inject Like button
-      var fb_url = base_url + kudo_id,
-      fb_like = '<div class="fb_like"><fb:like href="'+fb_url+'" layout="standard" show_faces="false" action="like" colorscheme="light"></fb:like></div>';
-      
-      $(this).after(fb_like);
-      
+      // Build and inject Like button
+        var fb_url = base_url + kudo_id,
+        fb_like = '<div class="fb_like"><fb:like href="'+fb_url+'" layout="standard" show_faces="false" action="like" colorscheme="light"></fb:like></div>';
+        
+        $("#facebook_like_" + kudo_id).html(fb_like);
+        
 
-    // Load in FB javascript SDK
-    $('body').append('<div id="fb-root"></div>');
-      window.fbAsyncInit = function() {
-      FB.init({appId: '152607901444320', status: true, cookie: true, xfbml: true});
-    };
-    (function() {
-      var e = document.createElement('script'); e.async = true;
-      e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-      document.getElementById('fb-root').appendChild(e);
-    }());
+      // Load in FB javascript SDK
+      $('body').append('<div id="fb-root"></div>');
+        window.fbAsyncInit = function() {
+        FB.init({appId: '152607901444320', status: true, cookie: true, xfbml: true});
+      };
+      (function() {
+        var e = document.createElement('script'); e.async = true;
+        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+        document.getElementById('fb-root').appendChild(e);
+      }());
+     //$(this).hide(); 
+
+    } else {
+      $(".plusone_button", this).show(); 
+      $(".facebook_like", this).show(); 
+    }
+    
     //////////////////////////////////
-    return false
+  }, function(event) {
+    $(".plusone_button", this).hide(); 
+    $(".facebook_like", this).hide(); 
   });
-
+  
 });
