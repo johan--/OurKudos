b0718bae32602698912cc2b02d47fd5855480196
Marcin Walczak <marcin.walczak@gmail.com>
2011-07-27 13:48:35 +0200

    Fixed another hoptoad bugs

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -10,6 +10,7 @@ class Admin::AdminController < ApplicationController
   private
 
     def check_index_permissions
+      redirect_to root_path if current_user.blank?
       redirect_to(home_url) unless current_user.has_role?(:admin) ||
             current_user.has_role?(:editor) ||
               current_user.has_role?("gift editor")
