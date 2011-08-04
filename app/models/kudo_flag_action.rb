27b73df545f95847d9e6cf0d4fa071b33cbe9de1
Charley Stran <charley.stran@gmail.com>
2011-08-04 16:51:02 -0500

    change you got kudo message

--- app/models/kudo_flag_action.rb
+++ app/models/kudo_flag_action.rb
@@ -3,7 +3,8 @@ class KudoFlagAction < ActiveRecord::Base
   belongs_to :admin_user,   :class_name => "User"
 
   ###Constants###
-  ACTIONS = [ ['Suspend User','suspend'],
+  ACTIONS = [ ['No Action', 'no_action'],
+              ['Suspend User','suspend'],
               ['Delete User', 'delete'],
               ['Message Author', 'message_author'],
               ['Message Recipients', 'message_recipients']]
