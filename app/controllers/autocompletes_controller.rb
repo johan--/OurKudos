91835f0b6921a813f310f4eb2f5f2686fec0ec8c
Charley Stran <charley.stran@gmail.com>
2011-08-22 18:58:21 -0500

    @sign idenitity fix

--- app/controllers/autocompletes_controller.rb
+++ app/controllers/autocompletes_controller.rb
@@ -71,8 +71,7 @@ class AutocompletesController < ApplicationController
 
     def autocomplete_identities_for_user
       friends = current_user.friendships.map{|f| f.friend_id}
-      Identity.where("user_id IN (?) AND identity_type = (?)",friends, 'twitter').map{ |i| i.identity}
-
+      Identity.where("user_id IN (?) AND identity_type = (?)",friends, 'twitter').map{ |i| "@#{i.identity}"}
     end
 
 end
