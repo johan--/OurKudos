a4b97aeb68749c7550c5c252ad719e77010d52b8
Charley Stran <charley.stran@gmail.com>
2011-09-03 17:04:38 -0500

    Messaging preferences specs

--- spec/models/messaging_preferences_spec.rb
+++ spec/models/messaging_preferences_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe MessagingPreference do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = Factory(:user)
+  end
+
+  it "should default system kudo email to true" do
+    @user.messaging_preference.system_kudo_email.should be(true)
+  end
+
+  it "should default weekly local summary to true" do
+    @user.messaging_preference.weekly_local_summary.should be(true)
+  end
+
+  it "should be able to update system kudo email preference" do
+    @user.messaging_preference.update_attribute('system_kudo_email', false)
+    @user.messaging_preference.system_kudo_email.should be(false)
+  end
+
+  it "should be able to update weekly local summary preference" do
+    @user.messaging_preference.update_attribute('weekly_local_summary', false)
+    @user.messaging_preference.weekly_local_summary.should be(false)
+  end
+
 end
