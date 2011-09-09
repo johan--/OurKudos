1477cb2572a8dbea2f37d5e1fcacdfdee26821fc
Charley Stran <charley.stran@gmail.com>
2011-09-09 18:13:43 -0500

    added display identity controlle specs

---
+++ spec/controllers/display_identities_controller_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+
+describe DisplayIdentitiesController do
+
+  before(:each) do 
+    @user = Factory(:user)
+    sign_in @user
+  end
+
+  
+
+  describe "editing an identity with valid params" do
+    before(:each) do 
+      @identity = mock_model(Identity, :user_id => @user.id, :update_attributes => true)
+      subject.current_user.identities.stub!(:find).with("1").and_return(@identity)
+    end
+
+    it "should have a current_user" do
+      subject.current_user.should_not be_nil
+    end
+
+    it "assigns the current users identities @identity" do
+      get :edit, :id => @user.id
+      assigns(:identities).should eq(@user.identities)
+    end
+
+    it "should have a flash notice" do
+      Identity.stub!(:update_display_identity).and_return(true)
+      put :update, :id => @user.id, :display_identity => 1
+      flash[:notice].should_not be_blank
+    end
+
+    it "should have a successful flash notice" do
+      Identity.stub!(:update_display_identity).and_return(true)
+      put :update, :id => @user.id, :display_identity => 1
+      flash[:notice].should eql('Display Account Updated!')
+    end
+
+    it "should redirect to the identities show page" do
+      Identity.stub!(:update_display_identity).and_return(true)
+      put :update, :id => @user.id, :display_identity => 1
+      response.should redirect_to(user_url(@user))
+    end
+  end
+
+  describe "updating a identity with invalid params" do
+    before(:each) do 
+      @identity = mock_model(Identity, :user_id => @user.id, :update_attributes => true)
+      subject.current_user.identities.stub!(:find).with("1").and_return(@identity)
+    end
+
+    it "should have a flash notice" do
+      Identity.stub!(:update_display_identity).and_return(false)
+      put :update, :id => @user.id, :display_identity => 1
+      flash[:error].should_not be_blank
+    end
+
+    it "should have a successful flash notice" do
+      Identity.stub!(:update_display_identity).and_return(false)
+      put :update, :id => @user.id, :display_identity => 1
+      flash[:error].should eql('There was a problem please try again')
+    end
+
+    it "should redirect to the identities show page" do
+      Identity.stub!(:update_display_identity).and_return(false)
+      put :update, :id => @user.id, :display_identity => 1
+      response.should redirect_to(edit_display_identity_url(@user))
+    end
+  end
+
+end
