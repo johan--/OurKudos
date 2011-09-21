acb0e5ea250eb2ca8a9afa549e81db36739da663
Ben Liu <blzabub8@gmail.com>
2011-09-21 17:32:54 -0400

    reverting attempted facebook share fix

--- lib/facebook_connection.rb
+++ lib/facebook_connection.rb
@@ -30,13 +30,11 @@ module OurKudos
 
    def post_to_friends_wall friend, kudo
      begin
-       #fb_friend = FbGraph::User.new(friend, :access_token => facebook_auth.token)
-       fb_friend = FbGraph::User.fetch(friend)
+       fb_friend = FbGraph::User.new(friend, :access_token => facebook_auth.token)
        result =    fb_friend.feed!(:message    => kudo.body,
                                    :link       => "http://preview.ourkudos.com/kudos/#{kudo.id}",
                                    :name        => 'OurKudos',
-                                   :description => "It's all good!",
-                                   :access_token => facebook_auth.token)
+                                   :description => "It's all good!")
         result.is_a?(FbGraph::Post)
      rescue Errno, Exception => e
        Rails.logger.info "SOCIAL_POSTING: Failed to post facebook kudo on friends wall #{e.to_s}"
