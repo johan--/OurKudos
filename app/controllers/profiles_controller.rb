3f227ce4008b98d43daaf211b4130c94f56d8513
Ben Liu <blzabub8@gmail.com>
2011-10-06 20:09:44 -0400

    sort member profile kudos by date, most recent to oldest

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,8 +14,8 @@ class ProfilesController < ApplicationController
     def get_member_kudos
 
       @kudos = @user.send("received_kudos") + @user.send("sent_kudos")
-      @kudos = @kudos.order("kudos.id DESC")       if @kudos.respond_to?(:order) && @kudos.first.is_a?(Kudo)
-      @kudos = @kudos.order("kudo_copies.id DESC") if @kudos.respond_to?(:order) && @kudos.first.is_a?(KudoCopy)
+      @kudos = @kudos.order("kudos.created_at DESC")       if @kudos.respond_to?(:order) && @kudos.first.is_a?(Kudo)
+      @kudos = @kudos.order("kudo_copies.updated_at DESC") if @kudos.respond_to?(:order) && @kudos.first.is_a?(KudoCopy)
   end
 
 
