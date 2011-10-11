7dc1817d6667852fea97e0775983bace20d71ffa
Ben Liu <blzabub8@gmail.com>
2011-10-11 13:58:52 -0400

    adding notification of vaild alternate sign in methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,6 +20,14 @@ class SessionsController < Devise::SessionsController
       send_failure_notice_to_admin(params[:user][:email], request.remote_ip, request.user_agent)
     end
     session['user.return_to'] = nil
+   
+    if resource.authentications.exists?
+      resource.authentications.each do |auth|
+      	flash[:notice] += " You can also sign in with your connected Twitter account." if auth.provider == "twitter"
+      	flash[:notice] += " You can also sign in with your connected Facebook account." if auth.provider == "facebook"
+      end
+      
+    end
   end
 
   def destroy
