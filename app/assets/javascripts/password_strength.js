26e96539ca26c9640fbd8860793038a5a2f6b7e0
Ben Liu <blzabub8@gmail.com>
2011-10-13 16:37:34 -0400

    adding password strength indicator to change password form

--- app/assets/javascripts/password_strength.js
+++ app/assets/javascripts/password_strength.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
 	jQuery('#signup_user_password').pstrength({minChar: 6, minCharText: ''});
+	jQuery('#change_user_password').pstrength({minChar: 6, minCharText: ''});
 
 }); // document.ready
