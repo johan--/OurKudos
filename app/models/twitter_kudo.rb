74973c18b5d4765e23f2ff704bd544dc491f15ac
Ben Liu <blzabub8@gmail.com>
2011-10-26 08:27:12 -0400

    fix for Twitter sharing notification bug per Charley

--- app/models/twitter_kudo.rb
+++ app/models/twitter_kudo.rb
@@ -36,6 +36,7 @@ class TwitterKudo < ActiveRecord::Base
   def send_system_email
     identity = Identity.where('identity = ? AND identity_type = ?', twitter_handle, 'twitter').first
     return false if identity.blank? 
+    return false if identity.user.id == author_id
     member  = identity.user
     UserNotifier.delay.social_system_kudo kudo.kudo, member if member.present?
   end
