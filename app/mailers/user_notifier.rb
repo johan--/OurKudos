efd268927cd151456899c48969d46ae21de74153
Charley Stran <charley.stran@gmail.com>
2011-11-12 09:02:26 -0600

    uncommented mailer line

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -102,7 +102,7 @@ class UserNotifier < ActionMailer::Base
     @user = username
     @ip = ip
     @user_agent = user_agent
-    #mail :to => 'charley.stran@gmail.com, ted@ourkudos.com', :subject => "Failed Login bad #{subject}"
+    mail :to => 'charley.stran@gmail.com, ted@ourkudos.com', :subject => "Failed Login bad #{subject}"
   end
 
   def login_failure_notify_user(user, ip_address)
