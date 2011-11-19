8536eb88724611b059f4ce7fff2e6b31052227b9
Charley Stran <charley.stran@gmail.com>
2011-11-18 18:23:31 -0600

    resizing for 4:3 ratio

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,7 @@
 class Attachment < ActiveRecord::Base
   has_attached_file :attachment, 
-                    :styles => {:thumb => "128x145>", :standard => "300x300>"}
+                    :styles => {:thumb => "128x145>", 
+                                :standard => "260x200>"}
   validates_attachment_presence :attachment
   validates_attachment_size :attachment, :less_than => 5.megabytes
   validates :name, :uniqueness => true
